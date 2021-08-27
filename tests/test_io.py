3d8bd7aba9b96bdb897b055dfe11a2c70f7339a0
Danila Bredikhin <danila.bredikhin@embl.de>
2021-08-27 19:08:15 +0200

    Modify IO tests for mudata

--- tests/test_io.py
+++ tests/test_io.py
@@ -6,8 +6,8 @@ import os
 import numpy as np
 from scipy.sparse import csr_matrix
 from anndata import AnnData
-import muon as mu
-from muon import MuData
+import mudata
+from mudata import MuData
 
 
 @pytest.fixture()
@@ -24,7 +24,7 @@ def mdata():
 class TestMuData:
     def test_write_read_h5mu_basic(self, mdata, filepath_h5mu):
         mdata.write(filepath_h5mu)
-        mdata_ = mu.read(filepath_h5mu)
+        mdata_ = mudata.read(filepath_h5mu)
         assert list(mdata_.mod.keys()) == ["mod1", "mod2"]
         assert mdata.mod["mod1"].X[51, 9] == pytest.approx(51.9)
         assert mdata.mod["mod2"].X[42, 18] == pytest.approx(959)
@@ -36,7 +36,7 @@ class TestMuDataMod:
         mdata.write(
             filepath_h5mu,
         )
-        mdata_ = mu.read_h5mu(filepath_h5mu, backed="r")
+        mdata_ = mudata.read_h5mu(filepath_h5mu, backed="r")
         assert list(mdata_.mod.keys()) == ["mod1", "mod2"]
 
         # When backed, the matrix is read-only
