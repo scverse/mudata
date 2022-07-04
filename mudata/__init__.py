864c0a27d85c719ddab7ed3da2097609efb83c8d
Danila Bredikhin <danila.bredikhin@embl.de>
2022-07-04 16:26:38 +0200

    v0.2.1

--- mudata/__init__.py
+++ mudata/__init__.py
@@ -5,6 +5,6 @@ from ._core import utils
 from ._core.io import *
 from ._core.config import set_options
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __anndataversion__ = "0.1.0"
 __mudataversion__ = "0.1.0"
