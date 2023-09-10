1e5bedadcaeeb41424ca641f46e7679fabc51d3b
Danila Bredikhin <danila.bredikhin@embl.de>
2023-09-10 04:10:14 +0200

    Drop dtype in AnnData constructors

--- mudata/_core/io.py
+++ mudata/_core/io.py
@@ -509,13 +509,14 @@ def _read_zarr_mod(g: zarr.Group, manager: MuDataFileManager = None, backed: boo
             d[k] = read_dataframe(g[k])
         elif k == "X":
             X = g["X"]
-            if isinstance(X, zarr.Group):
-                dtype = X["data"].dtype
-            elif hasattr(X, "dtype"):
-                dtype = X.dtype
-            else:
-                raise ValueError()
-            d["dtype"] = dtype
+            # No more dtype in anndata >=0.10
+            # if isinstance(X, zarr.Group):
+            #     dtype = X["data"].dtype
+            # elif hasattr(X, "dtype"):
+            #     dtype = X.dtype
+            # else:
+            #     raise ValueError()
+            # d["dtype"] = dtype
             if not backed:
                 d["X"] = read_elem(X)
         elif k != "raw":
@@ -550,13 +551,14 @@ def _read_h5mu_mod(
             d[k] = read_dataframe(g[k])
         elif k == "X":
             X = g["X"]
-            if isinstance(X, h5py.Group):
-                dtype = X["data"].dtype
-            elif hasattr(X, "dtype"):
-                dtype = X.dtype
-            else:
-                raise ValueError()
-            d["dtype"] = dtype
+            # No more dtype in anndata >=0.10
+            # if isinstance(X, h5py.Group):
+            #     dtype = X["data"].dtype
+            # elif hasattr(X, "dtype"):
+            #     dtype = X.dtype
+            # else:
+            #     raise ValueError()
+            # d["dtype"] = dtype
             if not backed:
                 d["X"] = read_elem(X)
         elif k != "raw":
