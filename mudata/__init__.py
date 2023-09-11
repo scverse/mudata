b903a477eaa3a57e38cd14734782b50ea233ba1c
Danila Bredikhin <danila.bredikhin@embl.de>
2023-09-11 15:13:41 +0200

    Start the main branch for v0.3

--- mudata/__init__.py
+++ mudata/__init__.py
@@ -2,9 +2,10 @@
 
 from ._core.mudata import MuData
 from ._core import utils
+from ._core.to_ import to_anndata, to_mudata
 from ._core.io import *
 from ._core.config import set_options
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 __anndataversion__ = "0.1.0"
 __mudataversion__ = "0.1.0"
