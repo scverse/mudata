9d8d9c9d953fd76d90a1e4b7ec99b850293aa8a7
Danila Bredikhin <danila.bredikhin@embl.de>
2023-09-11 15:21:53 +0200

    Add a missing import

--- mudata/__init__.py
+++ mudata/__init__.py
@@ -4,6 +4,7 @@ from ._core.mudata import MuData
 from ._core import utils
 from ._core.io import *
 from ._core.config import set_options
+from ._core.to_ import to_anndata, to_mudata
 
 __version__ = "0.2.3"
 __anndataversion__ = "0.1.0"
