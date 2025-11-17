dd212ed99f116472c1c8515e36786d95f870c0b2
Ilia Kats <ilia-kats@gmx.net>
2025-11-17 09:31:49 +0100

    silence ruff false positives

--- src/mudata/_core/mudata.py
+++ src/mudata/_core/mudata.py
@@ -657,8 +657,8 @@ class MuData:
             nonlocal index_order, can_update
             index_order = data_global.index.get_indexer(data_mod.index)
             can_update = (
-                new_idx.shape[0] == 0  # filtered or reordered
-                or kept_idx.shape[0] == data_global.shape[0]  # new rows only
+                new_idx.shape[0] == 0  # noqa: F821   filtered or reordered
+                or kept_idx.shape[0] == data_global.shape[0]  # noqa: F821     new rows only
                 or data_mod.shape[0]
                 == data_global.shape[
                     0
