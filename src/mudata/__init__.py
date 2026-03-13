ec8ccc2a9f0ffc2454957adf0900122b4cd47e8c
Ilia Kats <ilia-kats@gmx.net>
2026-03-13 15:55:15 +0100

    use scverse-misc for extensions

--- src/mudata/__init__.py
+++ src/mudata/__init__.py
@@ -1,10 +1,11 @@
 """Multimodal datasets"""
 
 from anndata import AnnData
+from scverse_misc import ExtensionNamespace
+from scverse_misc import make_register_namespace_decorator as _make_register_namespace_decorator
 
 from ._core import utils
 from ._core.config import set_options
-from ._core.extensions import ExtensionNamespace, register_mudata_namespace
 from ._core.io import (
     read,
     read_anndata,
@@ -25,6 +26,8 @@ from ._version import __version__, __version_tuple__
 # file format versions
 __anndataversion__ = "0.1.0"
 __mudataversion__ = "0.1.0"
+
+register_mudata_namespace = _make_register_namespace_decorator(MuData, "mdata", "register_mudata_namespace", "numpy")
 
 __all__ = [
     "__version__",
