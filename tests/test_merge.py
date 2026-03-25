f548fdde7c3d2a4ab26db4fcc2ee5b1af69d1c5e
Ilia Kats <ilia-kats@gmx.net>
2026-03-25 14:15:27 +0100

    apply review suggestions to merge tests

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -1,25 +1,16 @@
 import numpy as np
+import pytest
 
-import mudata
-
-N1 = 15
-
-
-def test_merge(mdata_nouniqueobs):
-    mdata1, mdata2 = mdata_nouniqueobs[:N1, :].copy(), mdata_nouniqueobs[N1:, :].copy()
-    mdata_ = mudata.concat([mdata1, mdata2])
-    assert list(mdata_.mod.keys()) == ["mod1", "mod2"]
-    for m in mdata_.mod_names:
-        assert mdata_.mod[m].shape == mdata_nouniqueobs.mod[m].shape
-    assert np.array_equal(mdata_.mod["mod1"].X, mdata_nouniqueobs.mod["mod1"].X)
-    assert np.array_equal(mdata_.mod["mod2"].X, mdata_nouniqueobs.mod["mod2"].X)
+import mudata as md
 
 
-def test_merge_and_write(mdata_nouniqueobs, filepath_h5mu):
-    mdata1, mdata2 = mdata_nouniqueobs[:N1, :].copy(), mdata_nouniqueobs[N1:, :].copy()
-    mdata_merged = mudata.concat([mdata1, mdata2])
-    mdata_merged.write_h5mu(filepath_h5mu)
-    mdata_ = mudata.read_h5mu(filepath_h5mu)
+@pytest.mark.parametrize("roundtrip", (False, True))
+def test_merge(mdata_nouniqueobs, filepath_h5mu, roundtrip):
+    mdata1, mdata2 = mdata_nouniqueobs[:15, :].copy(), mdata_nouniqueobs[15:, :].copy()
+    mdata_ = md.concat([mdata1, mdata2])
+    if roundtrip:
+        mdata_.write_h5mu(filepath_h5mu)
+        mdata_ = md.read_h5mu(filepath_h5mu)
     assert list(mdata_.mod.keys()) == ["mod1", "mod2"]
     for m in mdata_.mod_names:
         assert mdata_.mod[m].shape == mdata_nouniqueobs.mod[m].shape
